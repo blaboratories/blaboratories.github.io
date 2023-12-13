6d513acc2d876f58692d25b216dc42ed486cc58e
blaboratories <105821781+blaboratories@users.noreply.github.com>
2023-12-13 09:31:47 +0100

    Update script.js
    Logging

--- script.js
+++ script.js
@@ -1,58 +1,39 @@
 document.addEventListener('DOMContentLoaded', function() {
     const wordList = document.getElementById('wordList');
     let touchStart = 0;
-    let scrollStart = 0;
-    let scrollObservations = [];
+    let lastScrollTop = 0;
 
     // Populate the list with random words
     for (let i = 0; i < 2000; i++) {
         const word = document.createElement('div');
         word.className = 'word';
-        word.textContent = 'Word ' + i;
+        word.textContent = '2Word ' + i;
         wordList.appendChild(word);
     }
 
     wordList.addEventListener('touchstart', function(e) {
         touchStart = e.touches[0].clientY;
-        scrollStart = wordList.scrollTop;
+        lastScrollTop = wordList.scrollTop;
     }, false);
 
     wordList.addEventListener('touchend', function(e) {
         const touchEnd = e.changedTouches[0].clientY;
         if (Math.abs(touchStart - touchEnd) > 100) { // Detect a fast swipe
-            setTimeout(() => {
-                const scrollEnd = wordList.scrollTop;
-                if (scrollObservations.length < 2) {
-                    scrollObservations.push({ start: scrollStart, end: scrollEnd });
-                    markStopPoint(scrollEnd, 'STOP ' + (scrollObservations.length));
-                } else if (scrollObservations.length === 2) {
-                    const predictedStop = predictStopPoint(scrollObservations);
-                    insertForceItem(predictedStop);
-                }
-            }, 500); // Increased delay for better accuracy
+            // Immediately mark the current view area with "SCROLLED"
+            markScrollArea(lastScrollTop);
         }
     }, false);
 
-    function predictStopPoint(observations) {
-        const totalDistance = observations.reduce((acc, observation) => acc + (observation.end - observation.start), 0);
-        return totalDistance / observations.length + scrollStart;
-    }
-
-    function insertForceItem(position) {
-        const itemIndex = Math.round(position / 50); // Assuming each item is approximately 50px tall
-        const forceItem = wordList.children[itemIndex];
-        if (forceItem) {
-            forceItem.textContent = 'FORCE';
-            forceItem.style.color = 'blue'; // Optional: change color for visibility
-        }
-    }
-
-    function markStopPoint(position, text) {
-        const itemIndex = Math.round(position / 50); // Assuming each item is approximately 50px tall
-        const itemAtStop = wordList.children[itemIndex];
-        if (itemAtStop) {
-            itemAtStop.textContent = text;
-            itemAtStop.style.color = 'red'; // Optional: change color for visibility
+    function markScrollArea(scrollTop) {
+        const viewHeight = wordList.clientHeight;
+        const startIdx = Math.floor(scrollTop / 50);
+        const endIdx = Math.min(startIdx + Math.floor(viewHeight / 50), 2000);
+        for (let i = startIdx; i < endIdx; i++) {
+            const wordItem = wordList.children[i];
+            if (wordItem) {
+                wordItem.textContent = 'SCROLLED';
+                wordItem.style.color = 'green'; // Change color for visibility
+            }
         }
     }
 });
