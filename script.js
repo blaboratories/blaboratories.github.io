70099bc77a3e772fb991a05700243d842435ca20
blaboratories <105821781+blaboratories@users.noreply.github.com>
2023-12-13 09:17:10 +0100

    Update script.js
    Logging

--- script.js
+++ script.js
@@ -2,7 +2,6 @@ document.addEventListener('DOMContentLoaded', function() {
     const wordList = document.getElementById('wordList');
     let touchStart = 0;
     let scrollStart = 0;
-    let scrollEnd = 0;
     let scrollObservations = [];
 
     // Populate the list with random words
@@ -13,39 +12,39 @@ document.addEventListener('DOMContentLoaded', function() {
         wordList.appendChild(word);
     }
 
-    // Detect touch and scroll start
     wordList.addEventListener('touchstart', function(e) {
         touchStart = e.touches[0].clientY;
         scrollStart = wordList.scrollTop;
     }, false);
 
-    // Detect touch end and scroll end
     wordList.addEventListener('touchend', function(e) {
-        touchEnd = e.changedTouches[0].clientY;
+        const touchEnd = e.changedTouches[0].clientY;
         if (Math.abs(touchStart - touchEnd) > 100) { // Detect a fast swipe
             setTimeout(() => {
-                scrollEnd = wordList.scrollTop;
+                const scrollEnd = wordList.scrollTop;
+                console.log('Fast swipe detected. Start:', scrollStart, 'End:', scrollEnd);
                 if (scrollObservations.length < 2) {
                     scrollObservations.push({ start: scrollStart, end: scrollEnd });
+                    console.log('Observation recorded:', scrollObservations.length);
                 } else if (scrollObservations.length === 2) {
                     const predictedStop = predictStopPoint(scrollObservations);
+                    console.log('Predicted stop:', predictedStop);
                     insertForceItem(predictedStop);
                 }
-            }, 300); // Wait a bit for the scroll to stop
+            }, 300); // Adjust timing as needed
         }
     }, false);
 
     function predictStopPoint(observations) {
-        // Simple average calculation for prediction
         const totalDistance = observations.reduce((acc, observation) => acc + (observation.end - observation.start), 0);
         return totalDistance / observations.length + scrollStart;
     }
 
     function insertForceItem(position) {
-        // Logic to insert 'FORCE' item at the predicted position
         const forceItem = document.createElement('div');
         forceItem.className = 'word';
         forceItem.textContent = 'FORCE';
-        wordList.insertBefore(forceItem, wordList.children[Math.round(position / 50)]); // Assuming each item is approximately 50px tall
+        wordList.insertBefore(forceItem, wordList.children[Math.round(position / 50)]); // Adjust as per actual item height
+        console.log('FORCE inserted at position:', position);
     }
 });
