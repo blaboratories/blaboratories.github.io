de53c569e815af0f864dadc0a10486135b93c50c
blaboratories <105821781+blaboratories@users.noreply.github.com>
2023-12-18 20:02:57 +0100

    Update script.js
    console logging

--- script.js
+++ script.js
@@ -4,16 +4,14 @@ document.addEventListener('DOMContentLoaded', function() {
     let touchStartY = 0;
     let isScrolling = false;
     let sampleData = []; // Array to store samples
-    let deceleration = 0.0005; // Initial deceleration factor
     let predictionCount = 0; // To keep track of the number of predictions made
     const interval = 100; // 0.1 seconds in milliseconds
-    const someThreshold = 0.1; // Define a small value as a threshold
 
     // Populate the list with random words
     for (let i = 0; i < 4000; i++) {
         const word = document.createElement('div');
         word.className = 'word';
-        word.textContent = 'Word ' + i;
+        word.textContent = 'logWord ' + i;
         wordList.appendChild(word);
     }
 
@@ -43,20 +41,11 @@ document.addEventListener('DOMContentLoaded', function() {
                 if (newVelocity === 0) {
                     clearInterval(measurement);
                 }
-                const wordItem = wordList.children[predictionCount];
-                if (wordItem) {
-                    wordItem.textContent += predictionCount + ', ' + newTime + ', ' + newScrollTop + ', ' + newVelocity;
-                    wordItem.style.color = 'green';
-                }
+               // Log data for analysis
+                console.log(predictionCount + ', ' + newTime + ', ' + newScrollTop + ', ' + newVelocity);
                 // Check if scrolling has stopped
                 if (Math.abs(newVelocity) < 1) { // someThreshold is a small value
                     sampleData.push({ scrollTop: newScrollTop, time: newTime });
-                    //const itemIndex = Math.round(predictedStop / 42);
-                    const wordItem = wordList.children[predictionCount];
-                    if (wordItem) {
-                        wordItem.textContent += predictionCount + ', ' + newTime + ', ' + newScrollTop;
-                        wordItem.style.color = 'blue';
-                    }
                     if (sampleData.length === 100) {
                         //const predictedStop = estimateStoppingPosition(sampleData);
                         //markPredictedStop(predictedStop, predictionCount++);
